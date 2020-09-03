084644175c4dca0adae961203073371b4f88cdaf
Vikas_Datir <42106561+VIKAS-DATIR@users.noreply.github.com>
2020-09-03 09:52:48 +0530

    etl base classes for data extraction and transformation
    NA

---
+++ etl_classes/Extsitedata.py
@@ -0,0 +1,38 @@
+##################################################
+# This class use to extract data from the main
+# page of the moneycontrol website
+# This will create a file to store raw output data
+##################################################
+
+import requests as req
+import pandas as pd
+from datetime import datetime as dt
+from datetime import date as d
+
+class Extractsite:
+    def __init__(self, stgloc):
+        try:
+            self.URL= 'https://www.moneycontrol.com/'
+            self.dttime=dt.now().strftime("%Y-%m-%d_%H:%M:%S")
+            self.dtday =d.today().strftime("%a")
+            self.stgfile =stgloc+'StgStockData.txt'
+        except Exception as error:
+            print('Error in Extractsite - INIT '+ str(error))
+
+    def getdata(self):
+        try:
+            #getting data from the web site
+            self.rawdata = req.get(self.URL)
+            self.dflist = pd.read_html(self.rawdata.text)
+            self.rawdf = self.dflist[1]
+            #print(self.rawdf)
+
+            #adding timestamp part
+            self.moddf=pd.DataFrame(self.rawdf.values.tolist(),columns=['Index', 'Price', 'ChgAmt', 'ChgPer'])
+            self.moddf['Timestamp']=self.dttime
+            self.moddf['Day']=self.dtday
+
+            #writing data to text file
+            self.moddf.to_csv(self.stgfile, mode='w',sep='|', encoding='UTF-8',na_rep='NULL', index=False, header=True)
+        except Exception as error:
+            print('Error in Extractsite - STOREDATA '+ str(error))
---
+++ etl_classes/Transsitedata.py
@@ -0,0 +1,56 @@
+###################################################################
+# This class performs cleaning and formatting on stage file,
+# adds year, month and unique id
+###################################################################
+
+import csv
+from datetime import datetime as dt
+
+class Transformsite:
+    def __init__(self, stgloc, tgtloc='NA'):
+        try:
+            self.dstmp = dt.now().strftime("%Y-%m-%d_%H:%M:%S_%p")
+            self.stginfile=stgloc
+            self.tgtoutfile=tgtloc+'\\TgtStockData_'+(str(self.dstmp).replace(':',''))+'.txt'
+            self.rowid=0
+            self.rowlst=['INDEX_ID','INDEX_NAME','PRICE','CHANGE_AMT','CHANGE_PERCENT','TIMESTAMP','YEAR','MONTH','DAY']
+        except Exception as error:
+            print ('Error in Transsite Module - INIT ' +str(error))
+
+    def transdata(self):
+        try:
+            print(self.stginfile)
+            with open (self.stginfile,'r', encoding='UTF-8') as self.finptr:
+                with open(self.tgtoutfile,'w', encoding='UTF-8', newline='') as self.foutptr:
+                    self.fin  = csv.reader(self.finptr, delimiter='|')
+                    self.fout = csv.writer(self.foutptr, delimiter='|', quotechar=None)
+                    next(self.fin)
+                    self.fout.writerow(self.rowlst)
+                    self.rowlst.clear()
+                    for self.row in self.fin:
+                        if self.row[0].isspace() == False:
+                            self.rowid=self.rowid+1
+                            self.rowlst.append(self.row[4][0:4]+self.row[4][5:7]+self.row[4][8:10]+self.row[4][11:13]+str(self.rowid))
+                            self.rowlst.append(self.row[0])
+                            self.rowlst.append(float(self.row[1]))
+                            self.rowlst.append(float(self.row[2]))
+                            self.rowlst.append(float(self.row[3]))
+                            self.rowlst.append(self.row[4].replace('_',' '))
+                            self.rowlst.append(self.row[4][0:4])
+                            self.rowlst.append((dt.strptime(self.row[4][0:10],'%Y-%m-%d')).strftime("%B"))
+                            self.rowlst.append(self.row[5])
+                            self.fout.writerow(self.rowlst)
+                            self.rowlst.clear()
+                        #print(self.row[4][0:4]+self.row[4][5:7]+self.row[4][8:10]+self.row[4][11:13]+str(self.rowid))
+                        #print(self.row[0])
+                        #print(float(self.row[1]))
+                        #print(float(self.row[2]))
+                        #print(float(self.row[3]))
+                        #print(self.row[4].replace('_',' '))
+                        #print(self.row[4][0:4])
+                        #print((dt.strptime(self.row[4][0:10],'%Y-%m-%d')).strftime("%B"))
+                        #print(self.row[5])
+        except Exception as error:
+            print('Error in Transsite Module - TRANSDATA '+str(error))
+    def gettgtfile(self):
+        return self.tgtoutfile
